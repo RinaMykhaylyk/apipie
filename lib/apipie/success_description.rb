8051ba4a76c5fecf70bc710022eb9b0f412ca0d8
Iryna Mykhailyk <iryna.mykhailyk@onapp.com>
2025-04-28 16:35:08 +0300

    Add success and sample methods to api documentation

---
+++ lib/apipie/success_description.rb
@@ -0,0 +1,46 @@
+module Apipie
+  class SuccessDescription
+    attr_reader :code, :description, :metadata, :sample
+
+    def self.from_dsl_data(args)
+      code_or_options, desc, options = args
+      Apipie::SuccessDescription.new(code_or_options, desc, options)
+    end
+
+    def initialize(code_or_options, desc = nil, options = {})
+      if code_or_options.is_a? Hash
+        code_or_options.symbolize_keys!
+        @code = code_or_options[:code]
+        @metadata = code_or_options[:meta]
+        @description = code_or_options[:desc] || code_or_options[:description]
+        @sample = code_or_options[:sample]
+      else
+        @code =
+          if code_or_options.is_a? Symbol
+            begin
+              Rack::Utils.status_code(code_or_options)
+            rescue ArgumentError
+              'ApipieError: Bad use of success method.'
+            end
+          else
+            code_or_options
+          end
+
+        raise UnknownCode, code_or_options unless @code
+
+        @metadata = options[:meta]
+        @description = desc
+        @sample = code_or_options[:sample]
+      end
+    end
+
+    def to_json(lang)
+      {
+        :code => code,
+        :description => Apipie.app.translate(description, lang),
+        :metadata => metadata,
+        :sample => sample
+      }
+    end
+  end
+end
